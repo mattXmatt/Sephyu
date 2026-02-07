9abeacdefb3d92f71fa889989839455a731ba18c
Hajar <hajar.ahazzam@epitech.eu>
2026-02-07 11:16:05 +0100

    feat(world): world building with one character and one wall

--- sephyu/character/character_body_2d.gd
+++ sephyu/character/character_body_2d.gd
@@ -11,14 +11,14 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	var directions := Input.get_axis("up", "down")
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if directions:
-		velocity.y = directions * SPEED
+	var y_directions := Input.get_axis("up", "down")
+	var x_directions := Input.get_axis("ui_left", "ui_right")
+	if y_directions:
+		velocity.y = y_directions * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-	if direction:
-		velocity.x = direction * SPEED
+	if x_directions:
+		velocity.x = x_directions * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
